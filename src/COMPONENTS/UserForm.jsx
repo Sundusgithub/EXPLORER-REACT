049b296cb699b8765a4b906f30e88977869701d6
Sundus_Siddiqui <sundussddq@gmail.com>
2024-02-07 16:10:17 +0500

    FEAT:Setup,Components,Props,React-Icons,React-Boootstrap,HTMLtoJSX

--- src/COMPONENTS/UserForm.jsx
+++ src/COMPONENTS/UserForm.jsx
@@ -5,35 +5,35 @@ import React, { useState } from 'react'
 
 export default function UserForm() {
   // const email= "Sundussddq@gmail.com"
-const [email,setEmail] = useState("")
-const [password,setPassword] =useState("")
-// const demo =(value) => console.log(value)
-const userLogin =(e) => {
-e.preventDefault();
-const payload ={email,password}
-console.log(payload)
-}
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  // const demo =(value) => console.log(value)
+  const userLogin = (e) => {
+    e.preventDefault();
+    const payload = { email, password }
+    console.log(payload)
+  }
   return (
     <>
-        
-    <Form onSubmit={userLogin}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <Form.Text className="text-muted">
-       {email}
-        </Form.Text>
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}  />
-      </Form.Group>
-   
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+      <Form onSubmit={userLogin}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Form.Text className="text-muted">
+            {email}
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </>
   )
 }
